4cc25071c3e90c80ae7c15c5b79c3adcf0db478e
nick wise <nickmwise@gmail.com>
2021-01-23 16:06:00 -0700

    adding a new table called posts with columns

---
+++ CSC211-PHP_MYSQL/Week1/Scripts/NewPostsTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE posts(
+id INT auto_increment,
+user_id INT,
+title VARCHAR(100),
+body TEXT,
+publish_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(id),
+FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+# CREATES TABLE WITH FOREGIN KEY REFERENCE and publish_date DEFAULT CURRENT_TIMESTAMP
